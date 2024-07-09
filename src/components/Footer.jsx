d6371b18e87ec4c917e2ea7bd50323363ac64038
prakharmoses <me21b054@iittp.ac.in>
2024-07-09 17:29:40 +0530

    Just ignore

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,11 +37,11 @@ const Footer = () => {
   }
 
   return (
-    <footer class="bg-white rounded-lg shadow-md dark:shadow-none shadow-gray-600 m-4 dark:bg-gray-800 fixed bottom-[-0.5em] w-[97.7%]">
-      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; 2024 <a href="https://newsfeed.com/" class="hover:underline">ChainSphere</a>. All Rights Reserved.
+    <footer className="bg-white rounded-lg shadow-md dark:shadow-none shadow-gray-600 m-4 dark:bg-gray-800 fixed bottom-[-0.5em] w-[97.7%]">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; 2024 <a href="https://newsfeed.com/" className="hover:underline">ChainSphere</a>. All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <Link to="/aboutus" className="hover:underline me-4 md:me-6 cursor-pointer">About</Link>
           </li>
--- src/pages/Finance.jsx
+++ src/pages/Finance.jsx
@@ -1,24 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+// Importing IPFS for storage and related things
+import { ethers } from 'ethers';
+// import Web3Modal from 'web3modal';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
 export default function Finance() {
     const [hash, setHash] = useState('');
 
     const handleFileChange = async (event) => {
         const file = event.target.files[0];
-        const formData = new FormData();
-        formData.append('file', file);
-        try {
-            const response = await axios.post('http://127.0.0.1:8000/get_img_hash', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            });
-            const hash = response.data.output;
-            setHash(hash);
-        } catch (error) {
-            console.error(error)
-        }
+        console.log(client);
+        const add = await client.add(file);
+        const url = `https://infura.ipfs.io/ipfs/${add.path}`;
+        setHash(url);
     }
 
     return (
@@ -30,7 +27,7 @@ export default function Finance() {
                 requierd
                 onChange={handleFileChange}
             />
-            <p>{hash}</p>
+            {hash && <p>{hash}</p>}
         </main>
     )
 }
